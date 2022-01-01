7404551e62bfc480a6ac97542d2df7ae1bd20db2
Your Name <carolsantos14@hotmail.com>
2022-01-01 13:56:28 -0300

    storage firebase ok

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -1,32 +1,72 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Alert, FlatList } from 'react-native';
 
 import { Container, PhotoInfo } from './styles';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
-import { File } from '../../components/File';
-
-import { photosData } from '../../utils/photo.data';
+import { File, FileProps } from '../../components/File';
+import storage from '@react-native-firebase/storage';
 
 export function Receipts() {
+  const [ photos, setPhotos] = useState<FileProps[]>([]);
+  const [photoSelected, setPhotoSelected] = useState('')
+  const [photoInfo, setPhotInfo] = useState('')
+
+  async function handleShowImage(path: string){
+    const urlImage = await storage().ref(path).getDownloadURL();
+    setPhotoSelected(urlImage)
+    const info = await storage().ref(path).getMetadata()
+    setPhotInfo(`Upload realizado em ${info.timeCreated}`)
+  }
+
+  async function handleDeleteImage(path: string){
+    storage()
+      .ref(path)
+      .delete()
+      .then(() => {
+        Alert.alert('imagem excluida com sucesso!')
+        fetchImages() //pra recarregar nossa listagem    
+    })
+      .catch(error => console.error(error))
+  }
+
+  //funcao que busca pelas imagens
+  async function fetchImages(){
+    storage().ref('images').list().then(result => {
+      const files: FileProps[] = [];
+
+      result.items.forEach(file => {
+        files.push({
+          name: file.name,
+          path: file.fullPath
+        })
+      })
+      setPhotos(files);
+    })
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
   return (
     <Container>
       <Header title="Comprovantes" />
 
-      <Photo uri="" />
+      <Photo uri={photoSelected}/>
 
       <PhotoInfo>
-        Informações da foto
+        {photoInfo}
       </PhotoInfo>
 
       <FlatList
-        data={photosData}
+        data={photos}
         keyExtractor={item => item.name}
         renderItem={({ item }) => (
           <File
             data={item}
-            onShow={() => { }}
-            onDelete={() => { }}
+            onShow={() => handleShowImage(item.path)}
+            onDelete={() => handleDeleteImage(item.path)}
           />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
-
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Photo } from '../../components/Photo';
-import storage from '@react-native-firebase/app';
-
+import storage from '@react-native-firebase/storage';
 import { Container, Content, Progress, Transferred } from './styles';
+import { Alert } from 'react-native';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('')
+  const [progress, setProgress] = useState('0')
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -28,28 +29,43 @@ export function Upload() {
   };
 
   async function handleUpload(){
-    const fileName = new Date().getTime()
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/images/${fileName}.png`);
+    const uploadTask = reference.putFile(image)
 
+    //on vai acompanhar o progresso do upload da foto
+    //pega o total de bytes transferido e divide pela variavel que transferimos
+    uploadTask.on('state_changed', taskSnapShot => {
+      const percent = ((taskSnapShot.bytesTransferred / taskSnapShot.totalBytes)*100).toFixed(0)
+      setProgress(percent)
+      setBytesTransferred(`${taskSnapShot.bytesTransferred} transferido de ${taskSnapShot.totalBytes}`)
+    })
+
+    uploadTask.then(async() => {
+      const imageUrl = await reference.getDownloadURL()
+      console.log(imageUrl)
+      Alert.alert('upload Concluido com sucesso!')
+    })
   }
 
-  return (
+  return ( 
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
 
         <Button
           title="Fazer upload"
-          onPress={handlePickImage}
+          onPress={handleUpload}
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+         {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
